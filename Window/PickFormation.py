587f6a738811c98eadc75e8385f4d7f53505484d
Matthew Spencer <mspencer@softlayer.com>
2016-02-24 14:35:28 -0600

    Added an 'Any Formation' button to the pick formation screen. Updated the test players assigned to the teams.

--- Window/PickFormation.py
+++ Window/PickFormation.py
@@ -41,18 +41,21 @@ def open_pick_formation_window(window_x, window_y, db_dict, win_previous):
         win_pick_formation.become_target()
         AssignPlayers.open_assign_players_window(win_pick_formation.x, win_pick_formation.y,
                                                  db_dict, formation, win_pick_formation, roster={
-                'LW': db_dict['player_db'][1].db[3],
-                'ST': db_dict['player_db'][1].db[5],
+                'LW': db_dict['player_db'][1].db[1],
+                'ST': db_dict['player_db'][1].db[3],
                 'RW': db_dict['player_db'][1].db[0],
                 'LM': db_dict['player_db'][1].db[2],
-                'LCM': db_dict['player_db'][1].db[15],
-                'RCM': db_dict['player_db'][1].db[7],
-                'RM': db_dict['player_db'][1].db[35],
-                'LCB': db_dict['player_db'][1].db[18],
-                'CB': db_dict['player_db'][1].db[12],
-                'RCB': db_dict['player_db'][1].db[13],
-                'GK': db_dict['player_db'][1].db[4]})
+                'LCM': db_dict['player_db'][1].db[17],
+                'RCM': db_dict['player_db'][1].db[9],
+                'RM': db_dict['player_db'][1].db[40],
+                'LCB': db_dict['player_db'][1].db[13],
+                'CB': db_dict['player_db'][1].db[14],
+                'RCB': db_dict['player_db'][1].db[24],
+                'GK': db_dict['player_db'][1].db[5]})
         win_pick_formation.hide()
+
+    def any_formation_btn_func():
+        win_pick_formation.become_target()
 
     def back_btn_func():
         win_pick_formation.become_target()
@@ -61,7 +64,7 @@ def open_pick_formation_window(window_x, window_y, db_dict, win_previous):
 
     # ========== Formation Button Declarations ==========
     formation_button_width = 200
-    msg_y = title.bottom + title_border
+    msg_y = title.bottom
 
     for formation in db_dict['formation_db'][1].db:
         formation_btn = Button(title=formation['name'],
@@ -73,7 +76,16 @@ def open_pick_formation_window(window_x, window_y, db_dict, win_previous):
 
         msg_y += std_tf_height + 1
 
-    msg_y += 19
+    msg_y += 9
+
+    any_formation_btn = Button(title='Any Formation',
+                               font=std_tf_font,
+                               width=formation_button_width, height=std_tf_height,
+                               x=(win_pick_formation.width - formation_button_width)/2, y=msg_y,
+                               action=any_formation_btn_func)
+    general_display.append(any_formation_btn)
+
+    msg_y += std_tf_height + 10
 
     back_btn = Button('Back',
                       font=std_tf_font_bold,
